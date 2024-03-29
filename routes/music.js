fd6361d20874be2108b6252531f5aa6782d80b1f
ANISH BISWAS <iammanishbiswas@gmail.com>
2024-03-30 02:23:10 +0530

    Final eval

--- routes/music.js
+++ routes/music.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Music = require("../models/musicproducts");
 
+//adding a product
 router.post("/addproducts", async (req, res) => {
     try {
-        const { description , product_name , images , rating ,
-                reviews , price , colour , type , about , 
-                avalaibility , brand } = req.body;
+        const { description, product_name, images, rating,
+            reviews, price, colour, type, about,
+            avalaibility, brand } = req.body;
 
-        if(!description || !product_name || !images.length || !rating || !reviews || !price 
-            || !colour.length || !type || !about.length || !avalaibility || !brand)
-            {
-                res.json({
-                    message: "Please fill in all the fields!!",
-                    success: "false"
-                })
-            }
+        if (!description || !product_name || !images.length || !rating || !reviews || !price
+            || !colour.length || !type || !about.length || !avalaibility || !brand) {
+            res.json({
+                message: "Please fill in all the fields!!",
+                success: "false"
+            })
+        }
 
         const music_productData = new Music({
             description,
@@ -45,6 +45,23 @@ router.post("/addproducts", async (req, res) => {
 
 });
 
+//get product info
+router.get("/product-info/:prodId", async (req, res) => {
+    try {
 
+        const productid = req.params.prodId;
+
+        const prodInfo = await Music.findById(productid);
+
+        if (!prodInfo) {
+            res.status(400).json({ message: "No such products found!!", success: "false" });
+        }
+
+        res.json({ data: prodInfo, success: "true" });
+
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 module.exports = router;
